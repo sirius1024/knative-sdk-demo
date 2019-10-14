87bbccc70c837725fb70831da854de61b0aa6aa7
Sirius Guo <guotuo1024@gmail.com>
2019-10-14 15:33:53 +0800

    CRUD demo

--- go.mod
+++ go.mod
@@ -3,10 +3,14 @@ module github.com/sirius1024/knative-sdk-demo
 go 1.13
 
 require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/golang/groupcache v0.0.0-20191002201903-404acd9df4cc // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	go.uber.org/atomic v1.4.0 // indirect
 	go.uber.org/multierr v1.2.0 // indirect
 	go.uber.org/zap v1.10.0 // indirect
+	k8s.io/api v0.0.0-20190226173710-145d52631d00
+	k8s.io/apimachinery v0.0.0-20190221084156-01f179d85dbc
 	knative.dev/client v0.2.1-0.20191010130606-a2d1ef7d83f1
+	knative.dev/serving v0.8.0
 )
--- main.go
+++ main.go
@@ -3,15 +3,60 @@ package main
 import (
 	"fmt"
 
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"knative.dev/client/pkg/kn/commands"
+	servinglib "knative.dev/client/pkg/serving"
+	"knative.dev/serving/pkg/apis/serving/v1alpha1"
 )
 
 func main() {
-	// create a client
+	// get a service
 	p := commands.KnParams{}
 	p.Initialize()
 	client, _ := p.NewClient("default")
 	service, _ := client.GetService("helloworld-go")
-
 	fmt.Println(service.GetName())
+
+	// list services
+	serviceList, _ := client.ListServices()
+	for _, v := range serviceList.Items {
+		fmt.Println(v.GetName())
+	}
+
+	// create a service
+	var svcInstance = &v1alpha1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "guotuo-sdk-test3",
+			Namespace: "default",
+		},
+	}
+
+	svcInstance.Spec.Template = &v1alpha1.RevisionTemplateSpec{
+		Spec: v1alpha1.RevisionSpec{},
+		ObjectMeta: metav1.ObjectMeta{
+			Annotations: map[string]string{
+				servinglib.UserImageAnnotationKey: "",
+			},
+		},
+	}
+
+	// svcInstance.Spec.Template.Spec.PodSpec.Containers = []corev1.Container{{
+	// 	Image: "docker.io/lijiawang/helloworld-go:v1",
+	// 	Name:  "hwg",
+	// }}
+
+	svcInstance.Spec.Template.Spec.Containers = []corev1.Container{{Image: "guotuo1024/knative-web-demo:version-1.0.0"}}
+
+	// servinglib.UpdateImage(svcInstance.Spec.Template, "docker.io/guotuo1024/knative-web-demo:v1")
+
+	err := client.CreateService(svcInstance)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// Update
+	targetService, _ := client.GetService("guotuo-sdk-test3")
+	fmt.Println("Will update service " + targetService.GetName())
+	servinglib.UpdateImage(targetService.Spec.Template, "guotuo1024/knative-web-demo:version-2.0.0")
+	client.UpdateService(targetService)
 }
